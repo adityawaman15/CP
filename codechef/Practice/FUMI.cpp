2644a970f1ac23192e26e80976a4e9e4a1813961
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 21:31:36 +0530

    Solved: FUMI (codechef)

---
+++ codechef/Practice/FUMI.cpp
@@ -0,0 +1,13 @@
+// Problem: FUMI
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/FUMI
+// Solved on: 2026-09-23T16:01:35.397Z
+
+5
+1 4 7 3
+2 1 1 1
+5 1 2 2
+5 1 4 3
+12 7 9 4
