33db53daee7faa25c80c88c7e9b01412e96c17dd
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 20:18:47 +0530

    Solved: ECHOECHO (codechef)

---
+++ codechef/Practice/ECHOECHO.cpp
@@ -0,0 +1,8 @@
+// Problem: ECHOECHO
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/ECHOECHO
+// Solved on: 2026-09-23T14:48:47.011Z
+
+// source not captured automatically - open the solution page and copy it manually
