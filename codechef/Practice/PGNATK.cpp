ea38c2f3e3d6f2552f9813be481d6295b517959e
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 20:19:00 +0530

    Solved: PGNATK (codechef)

---
+++ codechef/Practice/PGNATK.cpp
@@ -0,0 +1,14 @@
+// Problem: PGNATK
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/PGNATK
+// Solved on: 2026-09-23T14:49:00.202Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+}
