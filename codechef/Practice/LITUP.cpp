5f97a92c618b5bdb95dba21fd46a420ab13026e3
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 20:25:38 +0530

    Solved: LITUP (codechef)

---
+++ codechef/Practice/LITUP.cpp
@@ -0,0 +1,16 @@
+// Problem: LITUP
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/LITUP
+// Solved on: 2026-09-23T14:55:37.643Z
+
+4
+5 1
+1 8 1 4 7
+3 1
+6 2 2
+6 3
+9 4 2 7 2 6
+7 1
+4 1 7 3 6 1 5
