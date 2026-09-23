6bfd4ec327419992384fbe3b3f2b530a93fcb602
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 20:55:34 +0530

    Solved: TWOPATH (codechef)

---
+++ codechef/Practice/TWOPATH.cpp
@@ -0,0 +1,31 @@
+// Problem: TWOPATH
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/TWOPATH
+// Solved on: 2026-09-23T15:25:34.292Z
+
+6
+2 2
+1 1
+1 1
+2 4
+1 0 0 0
+2 1 2 1
+3 5
+2 1 0 0 0
+1 1 1 1 1
+0 0 1 1 2
+4 3
+2 0 2
+0 0 0
+2 0 2
+2 0 2
+4 5
+1 1 0 0 0
+1 1 1 0 0
+0 0 1 1 0
+0 0 0 1 1
+2 6
+2 2 2 2 2 2
+2 2 2 2 2 2
