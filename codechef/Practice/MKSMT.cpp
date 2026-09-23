109280f16b37683c852886677a63b950c66884fa
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 21:29:10 +0530

    Solved: MKSMT (codechef)

---
+++ codechef/Practice/MKSMT.cpp
@@ -0,0 +1,14 @@
+// Problem: MKSMT
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/START257D/problems/MKSMT
+// Solved on: 2026-09-23T15:59:10.013Z
+
+3
+4
+2 4 4 7
+5
+2 7 3 3 9
+6
+1 2 10 3 4 20
