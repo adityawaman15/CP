fbcfe0d7e999e39e09c59a9598f8e3252b0a7516
Aditya Shrikant Waman <aditya.waman25@pccoepune.org>
2026-09-23 18:44:47 +0530

    Solved: WATERCOOLER2 (codechef)

---
+++ codechef/Practice/WATERCOOLER2.cpp
@@ -0,0 +1,29 @@
+// Problem: WATERCOOLER2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/WATERCOOLER2
+// Solved on: 2026-09-23T13:14:46.259Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    long long T;
+    cin >> T;
+    
+    while(T--){
+        long long X,Y;
+        cin >> X >> Y;
+        
+        cout << (Y-1)/X << "\n";
+        
+        
+    }
+    
+    
+
+}
